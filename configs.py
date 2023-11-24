bc268807b6e5b186c5be9ecdad45d50c370121a0
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2023-11-24 12:34:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ class Config(object):
 	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://dohoj11515:f5VibAbQ4LVHpQ5n@cluster0.h3s7s7p.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "1907166812")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "2088041233")
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
